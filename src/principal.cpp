a8b30e8d38f03a043152bd72f5706c0300e749ac
argenton <joaopedroargenton@hotmail.com>
2024-05-07 18:37:50 -0300

    criaçao de txt para colocar comandos sfml // criaçao de janela e retangulo teste

--- src/principal.cpp
+++ src/principal.cpp
@@ -9,3 +9,26 @@ Principal::Principal(/* args */):
 Principal::~Principal()
 {
 }
+void Principal::executar(){
+    //criando um obj na (retangulo)
+    sf::RectangleShape jogador(sf::Vector2f(50.0f, 50.0f)); /* cria um obj retangular com as proporçoes indicadas*/
+    jogador.setFillColor(sf::Color::Blue); /* define a cor do jogador*/
+    jogador.setPosition(50.0f,50.0f); // altera posiçao do jogador
+    while (window.isOpen()) // propriedade da classe window, que verifica se a mesma esta aberta
+    {
+        sf::Event evento; // cria obj classe evento
+        if(window.pollEvent(evento)){ //propriedade classe window que verifica eventos
+            if(evento.type == sf::Event::Closed){//evento tem uma propriedade typo, que pode ser verificada com outros metodos da classe
+                window.close(); // metodo classe window
+            } else if(evento.type == sf::Event::KeyPressed){ // a classe evento possui uma outra classe, que é um tipo de evento, que ppode ser uma chave press
+                if(evento.key.code == sf::Keyboard::Escape){ // sendo uma chave, o obj evento/keypressed possui um mtodo code, que le a tecla desejada
+                    window.close();
+                }
+            }
+        } 
+        window.clear(); // limpa a tela anterior
+        window.draw(jogador); //desenha um obj
+        window.display(); /*atualiza a tela, mostrando todos os obj desenhados*/
+    }   
+    
+}
